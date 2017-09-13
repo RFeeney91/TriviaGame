4321f7e92f23bcc6ddae5026cccdf1e4324fed7f
RFeeney91 <ryansfeeney@gmail.com>
2017-09-13 16:24:49 -0700

    upated trivia

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,26 @@
+var span = document.querySelector("#count");
 
+function countDown(counter){
+
+    var interval = setInterval(function(){
+        
+        var minutes = ((counter / 60) | 0) + "";
+        var seconds = (counter % 60) + "";
+        var format = ""
+                      + new Array(3-minutes.length).join("0") + minutes 
+                      + ':' 
+                      + new Array(3-seconds.length).join("0") + seconds;
+
+        span.innerHTML = format;
+
+        counter--;
+
+        if(counter === 0){
+            clearInterval(interval);
+            alert('sorry, out of time')
+        }
+        
+    },1e3)
+}
+
+countDown(120);
